0e40c835ae77d08daffafb83eefbd1cee00e7ef8
Jonas Bronselaer <55691421+JonasBronselaer@users.noreply.github.com>
2020-03-05 12:27:02 +0100

    nieuwe repository dwd

---
+++ Les1-2/js/scripts.js
@@ -0,0 +1,4 @@
+let sexuality = 'gay'
+console.log('Liam ' + sexuality)
+document.getElementById('int').addEventListener('click', function() {
+      inpAnswer.className = inpAnswer.value == val1 + val2 ? 'right' : 'wrong';
